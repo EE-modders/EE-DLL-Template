3fcf9f6a981d75c6c035163d96560a065cf4f85b
EnergyCube <dev@energycube.fr>
2022-08-31 22:55:23 +0200

    Added log file size limit

--- EE_DLL_Template/EE_DLL_Template/Project.h
+++ EE_DLL_Template/EE_DLL_Template/Project.h
@@ -6,7 +6,7 @@
 #include <string>
 
 // Project INFO
-static const std::string PROJECT_NAME_STR = "Project";			// Also used as log file name!
+static const std::string PROJECT_NAME_STR = "Project";
 static const std::string PROJECT_AUTHOR_STR = "YourPseudo";
 static const std::string PROJECT_VERSION_STR = "1.0.0";
 static const unsigned int PROJECT_VERSION_MAJOR = 1;
@@ -15,8 +15,8 @@ static const unsigned int PROJECT_VERSION_PATCH = 0;
 // END Project INFO
 
 // Hard Coded Project Settings
-static bool KEEP_ALIVE = true;		// Use => true:		the DLL will wait until game stop to call onStop()
-									//     => false:	the DLL will call onStop() (Detach) if onStart() ended and didn't
+static bool KEEP_ALIVE = true;		// Use => true:		the DLL will wait until game stop to call onStop() (Detach)
+									//     => false:	the DLL will call onStop() if onStart() ended and didn't
 									//					created any additional thread (so, if the DLL is fully inative)
 
 class Project
